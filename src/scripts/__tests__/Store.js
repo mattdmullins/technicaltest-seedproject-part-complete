f0c8e9a9ab3c3a8d5e81ee129a5a09a65d3e0386
Matt Mullins <matdav@gmail.com>
2021-01-04 21:10:15 +0000

    tests setup

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -2,15 +2,98 @@ import Store from "../Store";
 import mockData from "../../../public/db.json";
 
 describe("filter", () => {
-  it("should return all deals when no filters applied", () => {
+  let sut;
+  let mockDeals;
+  beforeEach(() => {
+    mockDeals = mockData.deals;
     // Arrange
-    const sut = new Store();
-    sut.setDeals(mockData.deals);
-
+    sut = new Store();
+    sut.setDeals(deals);
+  });
+  it("should return all deals when no filters applied", () => {
     // Act
     const result = sut.deals;
 
     // Assert
     expect(result).toEqual(mockData.deals);
+  });
+
+  it("show the broadband only deals when broadband filter applied", () => {
+    // Act
+    sut.setProductFilter("broadband");
+    const matchedDeals = sut.deals;
+    const matchedDealsWithoutFilter = matchedDeals.filter((deal) => {
+      const payload =
+        deal.productTypes.includes("Broadband") ||
+        deal.productTypes.includes("Fibre Broadband");
+      return !payload;
+    });
+
+    // Assert
+    expect(matchedDeals).toHaveLength(4);
+    expect(matchedDealsWithoutFilter.length).toBeFalsy();
+  });
+
+  it("should return the deals for broadband and tv only when broadband and tv filters applied", () => {
+    // Act
+    sut.setProductFilter("broadband");
+    const matchedDeals = sut.deals;
+    const matchedDealsWithoutFilter = matchedDeals.filter((deal) => {
+      const payload =
+        (deal.productTypes.includes("Broadband") ||
+          deal.productTypes.includes("Fibre BroadBand")) &&
+        deal.productTypes.includes("TV");
+      return !payload;
+    });
+
+    // Assert
+    expect(matchedDeals).toHaveLength(4);
+    expect(matchedDealsWithoutFilter.length).toBeFalsy();
+  });
+  it("should return the deals for broadband and mobile only when broadband and mobile filters applied", () => {
+    // Act
+    sut.setProductFilter("broadband");
+    const matchedDeals = sut.deals;
+    const matchedDealsWithoutFilter = matchedDeals.filter((deal) => {
+      const payload =
+        (deal.productTypes.includes("Broadband") ||
+          deal.productTypes.includes("Fibre BroadBand")) &&
+        deal.productTypes.includes("Mobile");
+      return !payload;
+    });
+
+    // Assert
+    expect(matchedDeals).toHaveLength(1);
+    expect(matchedDealsWithoutFilter.length).toBeFalsy();
+  });
+  it("should return the deal for Sky only when Sky filter applied", () => {
+    // Act
+    sut.setProductFilter("broadband");
+    const matchedDeals = sut.deals;
+    const matchedDealsWithoutFilter = matchedDeals.filter((deal) => {
+      const payload = deal.provider.name === "Sky";
+      return !payload;
+    });
+
+    // Assert
+    expect(matchedDeals).toHaveLength(1);
+    expect(matchedDealsWithoutFilter.length).toBeFalsy();
+  });
+  it("should return the deals for BT with broadband and tv only when BT, broadband, and tv filters applied", () => {
+    // Act
+    sut.setProductFilter("broadband");
+    const matchedDeals = sut.deals;
+    const matchedDealsWithoutFilter = matchedDeals.filter((deal) => {
+      const payload =
+        (deal.productTypes.includes("Broadband") ||
+          deal.productTypes.includes("Fibre BroadBand")) &&
+        deal.productTypes.includes("TV") &&
+        deal.provider.name === "BT";
+      return !payload;
+    });
+
+    // Assert
+    expect(matchedDeals).toHaveLength(2);
+    expect(matchedDealsWithoutFilter.length).toBeFalsy();
   });
 });
